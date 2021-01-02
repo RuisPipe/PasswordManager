655b191f21c590cfd2f179668acbac7ea2d6efb5
RuisPipe <alexander@roese.dev>
2021-01-02 20:06:58 +0100

    Added timestamp parameter to getDate function

--- app.js
+++ app.js
@@ -136,9 +136,9 @@ function getFullTime(timestamp) {
   });
 }
 
-function getDate(symbol) {
+function getDate(symbol, timestamp) {
   return new Promise((resolve) => {
-    const today = new Date();
+    const today = timestamp !== undefined ? new Date(timestamp) : new Date();
 
     const day = ('0' + today.getDate()).slice(-2);
     const month = ('0' + (today.getMonth() + 1)).slice(-2);
